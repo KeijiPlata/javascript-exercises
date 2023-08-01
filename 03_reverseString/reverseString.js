b630b0545cf9eef9d39827e7d8f56eaf1428a570
KeijiPlata <lordmiackykeijip@gmail.com>
2023-08-01 14:07:15 +0800

    finish 03 reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    // add container of new word
+    let newString = "";
 
+    // split the text and store to array
+    const ArrayText = text.split("");
+
+    // just like the countdown, decrement the index and store it to container
+    for (let index = ArrayText.length - 1; index >= 0; index--) {
+        newString += ArrayText[index];
+        
+    }
+
+    // return new word
+    return newString;
 };
 
 // Do not edit below this line
