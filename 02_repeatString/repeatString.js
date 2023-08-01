e11ec83845c646dbe766267f85cd39ac0e6de36f
KeijiPlata <lordmiackykeijip@gmail.com>
2023-08-01 13:04:08 +0800

    finish 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(stringText, numberOfRepeat) {
+    let newString = ""
+    if (numberOfRepeat < 0){
+        return "ERROR"
+    }
+    else{
+    for (let index = 0; index < numberOfRepeat; index++) {
+        newString = newString + stringText;
+        
+    }
 
+    return newString;
+}
 };
 
 // Do not edit below this line
