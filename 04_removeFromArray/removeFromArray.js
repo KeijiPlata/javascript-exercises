29ca2bad7ba7d1c830d6167b898424408b10684a
KeijiPlata <lordmiackykeijip@gmail.com>
2023-08-02 10:54:59 +0800

    finish activity 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    /// ...remove called rest parameters, it will take unlimited arguments and put in a array
+    // for each loop to array remove, and check if element in array is equal to remove element
+    // if true, remove the array item using splice
+    remove.forEach((removeElement) => {
+        array.forEach((element, i) => {
+            if(element === removeElement){
+                // in splice function, we taking 3 arguments, the starting index, 
+                // delete count (how many to be deleted), and what to add.
+                array.splice(i, 1);
+            }
+            
+        });
+    });
 
+    return array;
 };
 
 // Do not edit below this line
