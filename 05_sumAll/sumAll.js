659083560d92e659b0c293c9a6c17332a9655eb3
KeijiPlata <lordmiackykeijip@gmail.com>
2023-08-03 14:05:19 +0800

    finish 05 sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let totalSum = 0;
+    let temp = 0;
 
+    if(start < 0 || end < 0 || typeof start != 'number' || typeof end != 'number'){
+        return "ERROR"
+    }
+
+    if(start > end){
+        temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for (let index = start; index <= end; index++) {
+        totalSum += index;
+        
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
