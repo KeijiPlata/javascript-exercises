b070b54c726c802beb1946d246d1ec4eb11eab6f
KeijiPlata <lordmiackykeijip@gmail.com>
2023-08-03 14:48:28 +0800

    add comments to 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,21 @@
 const repeatString = function(stringText, numberOfRepeat) {
+    // container of new string
     let newString = ""
+
+    // validation if number of repeat is less than 0, print ERROR
     if (numberOfRepeat < 0){
         return "ERROR"
     }
+
+    // loop if passed the validation
     else{
     for (let index = 0; index < numberOfRepeat; index++) {
+        // concat word
         newString = newString + stringText;
         
     }
 
+    // return new word
     return newString;
 }
 };
