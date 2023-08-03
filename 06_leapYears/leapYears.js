4851696b54b30a950b2f759226494a449894d7c3
KeijiPlata <lordmiackykeijip@gmail.com>
2023-08-03 14:24:25 +0800

    finish 06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    // checks if years is divisible by 4
+    if(years % 4 === 0){
+        // checks if years is divisible by 100
+        if(years % 100 === 0){
+            // checks if years is divisible by 400
+            if(years % 400 === 0){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
